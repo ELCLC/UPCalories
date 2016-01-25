e1030adfe7fe7e7be1be45dfc0ce4d37461344dc
Emma Lee-Choon <eleechoon@gmail.com>
2016-01-25 18:09:18 +0000

    Add tests for AppDelegate and ViewController

---
+++ UPCalories/Api.swift
@@ -0,0 +1,13 @@
+//
+//  Api.swift
+//  UPCalories
+//
+//  Created by Emma Lee-Choon on 24/01/2016.
+//  Copyright © 2016 Emma Lee-Choon. All rights reserved.
+//
+
+import UIKit
+
+class Api: NSObject {
+
+}
